1da0bb6d5c2bd63ef66ad9e12dc5850b50ed2cbb
guluehmedova <guliehmedova19@gmail.com>
2023-10-01 19:13:07 +0400

    footer

--- manage-page/main.js
+++ manage-page/main.js
@@ -6,6 +6,7 @@ navToggle.addEventListener('click', () => {
     primaryNav.hasAttribute('data-visible')
         ? navToggle.setAttribute('aria-expanded', false) :
         navToggle.setAttribute('aria-expanded', true);
+            
     primaryNav.toggleAttribute('data-visible');
     primaryHeader.toggleAttribute('data-overlay');
 });
