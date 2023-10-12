e27425641a2bba6fa2e227593d8193b30a4a302c
guluehmedova <guliehmedova19@gmail.com>
2023-10-12 11:16:40 +0400

    slider

--- manage-page/main.js
+++ manage-page/main.js
@@ -6,7 +6,21 @@ navToggle.addEventListener('click', () => {
     primaryNav.hasAttribute('data-visible')
         ? navToggle.setAttribute('aria-expanded', false) :
         navToggle.setAttribute('aria-expanded', true);
-            
+
     primaryNav.toggleAttribute('data-visible');
     primaryHeader.toggleAttribute('data-overlay');
+});
+
+
+/*SLIDER */
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: false,
+    dots: true,
+    centerMode: true,
+    arrows: false,
+    responsive: {
+        480: {
+            dots: false
+        }
+    }
 });
